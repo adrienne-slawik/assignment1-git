248ebc51123360d79c72b996a962a39cfe4857cb
adrienne.slawik <adrienne.slawik@plu.edu>
2021-03-10 23:27:06 -0800

    4a

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("I am very tired, idk what this is anymore
-        .");;
+        . Whyyyyy");;
         }
     } //end of Hello.java
